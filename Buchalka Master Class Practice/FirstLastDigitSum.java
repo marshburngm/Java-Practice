885affaeb2eb2ff4589b46e3d7571c89ee68e4e3
marshburngm <100003121+marshburngm@users.noreply.github.com>
2022-05-18 20:05:41 -0400

    Create FirstLastDigitSum.java

---
+++ Buchalka Master Class Practice/FirstLastDigitSum.java
@@ -0,0 +1,18 @@
+public class FirstLastDigitSum {
+    
+    public static int sumFirstAndLastDigit(int number){
+        if(number < 0){
+            return -1;
+        }
+
+        int lastDigit =  number % 10;
+
+        while(number >= 10){
+            number /= 10;
+        }
+        int firstDigit = number;
+
+        return firstDigit + lastDigit;
+
+    }
+}
